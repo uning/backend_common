bafe9e9991485bd59a55e4a78c3a3f9b58bd1e98
Jimmy <zxm@playcrab.com>
2010-06-04 15:42:17 +0800

    fix

--- phplib/renren/config.deploy.php
+++ phplib/renren/config.deploy.php
@@ -5,9 +5,9 @@ class RenrenConfig
     public static $secret = "ca22f87355ed4b9fb101659300282b47";
     public static $callback_url = "http://poker.playcrab.com/";
     public static $resource_url = "http://poker.playcrab.com/";
-    public static $canvas_name = "texas";
+    public static $canvas_name = "poker_funny";
     public static $canvas_host  = "http://apps.renren.com/";
-	public static $mobile_canvas_name = "texas";
+	public static $mobile_canvas_name = "poker_funny";
 	public static $mobile_canvas_host = "http://mapps.renren.com/";
     public static $group_url = "http://group.renren.com/GetTribe.do?id=301354649";
     public static $pay_secure = 'a0901b';
