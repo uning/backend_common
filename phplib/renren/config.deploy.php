bf03257a2cee7c2f62daacbea87cef0f089f5a9a
Jimmy <zxm@playcrab.com>
2010-06-04 12:04:50 +0800

    config

--- phplib/renren/config.deploy.php
+++ phplib/renren/config.deploy.php
@@ -1,14 +1,15 @@
 <?php
-
 class RenrenConfig
 {
-    public static $api_key = "975b170745ce4b4d9fe138df194a5535";
-    public static $secret = "411cd6a100584d50b8d93bd603f26ceb";
-    public static $callback_url = "http://s1.coolplusplus.com/bg/renren/";
-	public static $resource_url = "http://files5.qq494.cn/pig/hotel/";
-	public static $canvas_name = "happyhotel";
+    public static $api_key = "8190bad9d1cc4073aaee858b5c03dc54";
+    public static $secret = "ca22f87355ed4b9fb101659300282b47";
+    public static $callback_url = "http://poker.playcrab.com/";
+    public static $resource_url = "http://poker.playcrab.com/";
+    public static $canvas_name = "texas";
     public static $canvas_host  = "http://apps.renren.com/";
-    public static $group_url = "http://apps.renren.com/happyhotel";
-	public static $pay_secure = 'asdfasifeaifas';
+	public static $mobile_canvas_name = "texas";
+	public static $mobile_canvas_host = "http://mapps.renren.com/";
+    public static $group_url = "http://group.renren.com/GetTribe.do?id=301354649";
+    public static $pay_secure = 'a0901b';
+    public static $app_id = 100762;
 }
-
