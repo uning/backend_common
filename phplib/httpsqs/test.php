569ca72a8537cdac4798c4411a7f97559a2b02d9
hotel@ldev <hotel@ldev>
2010-11-23 12:05:26 +0800

    for dev.tingkun.com

--- phplib/httpsqs/test.php
+++ phplib/httpsqs/test.php
@@ -4,7 +4,8 @@ require_once('AsyncQueue.php');
 $aq = new AsyncQueue();
 
 $i = 1;
-$aq->put($i++);
+$aq->put(array($i++));
+$aq->put(array($i++));
 $aq->put($i++);
 $aq->put($i++);
 print_r($aq->status());
